597a5c669826cf65c79d567908614a20e1ac3c49
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-07-14 23:53:20 +0600

    added dart_build_types

---
+++ dart_build_types/bin/dart_build_types.dart
@@ -0,0 +1,66 @@
+void main01(List<String> arguments) {
+  String str01 = 'Kurs15';
+  print(str01);
+
+  num num01 = 12.90;
+  print(num01);
+
+  int int01 = 13;
+  print(int01);
+
+  double double01 = 12.12;
+  print(double01);
+
+  bool bool01 = true; // false
+  print(bool01);
+
+  print('$str01, $num01, $int01, $double01, $bool01');
+
+  // String example
+  String firstName = 'Semetei';
+  String lastName = 'Manas';
+  print('Menin atym $firstName, atamdyn aty $lastName');
+
+  // Num example
+  num san1 = 47;
+  num san2 = 54;
+  print('$san1+$san2=${san1 + san2}');
+
+  // Bool example
+  bool correct = true;
+  bool unCorrect = false;
+  print('1+1=2 $correct');
+  print('1+3=2 $unCorrect');
+
+  // Dynamic
+  dynamic dynamic01 = 'Seitek';
+  print(dynamic01);
+
+  // var, char,
+  // var ozgormonu ataganda koldonobuz
+}
+
+void main02(List<String> args) {
+  // 1) formula
+  // Type variable-name = variable-value
+  String name1 = 'Eldiiar';
+  int jash2 = 40;
+  // 2 formula
+  // var variable-name = variable-value
+  var name2 = 'Flutter';
+  var jash1 = 40;
+}
+
+void main(List<String> args) {
+  // 1: 1 {function} je bolboso bir {class} ichinde okshosh attagy ozgorulmo tuzo albaibyz
+  var name = 'Name';
+  String name2 = 'Name';
+
+  // 2: birinchi soz kichine tamga menen kiyinki sozdor chong menen ozgorulmonun attary
+  num userCount = 1234;
+
+  // 3: Type berilgen var kiyin type ozgorboit
+  int mount = 1234;
+  mount = 4321;
+  // mount = 'Mount';
+}
