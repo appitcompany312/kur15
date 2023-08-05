29a4cbde78811444312b7870aa6f12f1413a8315
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-08-06 00:02:10 +0600

    finish auth mehtod

--- dart_control_flow_statements02/bin/dart_control_flow_statements02.dart
+++ dart_control_flow_statements02/bin/dart_control_flow_statements02.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
-Map user = {
-  'name': 'User1',
-  'lastname': 'User1User',
-  'age': 33,
-  'login': 'user1@gmail.com',
-  'password': '1234',
-};
+// Map user = {
+//   'name': 'User1',
+//   'lastname': 'User1User',
+//   'age': 33,
+//   'login': 'user1@gmail.com',
+//   'password': '1234',
+// };
 
 void main(List<String> arguments) {
   authentication();
@@ -22,54 +22,78 @@ void main(List<String> arguments) {
 // 7) Eger kata bolso login & password kata jazat
 
 void authentication() {
-  // Salamdashuu
-  print('"Flutter 15" gruppasyna kosh keldiniz!');
+  var userler = [];
+  var quit = '0';
 
-  // Login/Register
-  print('Kattaluu uchun "Y". Kiruu uchun "N" jazynyz');
-  var value = stdin.readLineSync();
+  do {
+    // Salamdashuu
+    print('"Flutter 15" gruppasyna kosh keldiniz!'); //  bolgonu bir print()
 
-  if (value == 'Y') {
-    print('Siz kattaluu bolumundosuz');
+    // Login/Register
+    print('Kattaluu uchun "Y". Kiruu uchun "N" jazynyz');
+    var value = stdin.readLineSync();
 
-    stdout.write("Atynyzdy jazynyz : ");
-    var name = stdin.readLineSync();
+    if (value == 'Y') {
+      print('Siz kattaluu bolumundosuz');
 
-    stdout.write("Familiyanyzdy jazynyz : ");
-    var lastName = stdin.readLineSync();
+      stdout.write("Atynyzdy jazynyz : ");
+      var name = stdin.readLineSync();
 
-    stdout.write("Jashynyzdy jazynyz : ");
-    var age = stdin.readLineSync();
+      stdout.write("Familiyanyzdy jazynyz : ");
+      var lastName = stdin.readLineSync();
 
-    stdout.write("Login jazynyz : ");
-    var login = stdin.readLineSync();
+      stdout.write("Jashynyzdy jazynyz : ");
+      var age = stdin.readLineSync();
 
-    stdout.write("Syr soz jazynyz : ");
-    var passoword = stdin.readLineSync();
+      stdout.write("Login jazynyz : ");
+      var login = stdin.readLineSync();
 
-    // Fixme: bulardy irettesh kerek
-    print('Name is $name');
-    print('Lastname is $lastName');
-    print('Age is $age');
-    print('Login is $login');
-    print('Pasword is $passoword'); // Fixme (passiword korunbosh kerek)
-    // Todo(edliiar): userdi saktoo
-    print('Siz iygiliktuu kattaldynyz :)');
-  } else if (value == 'N') {
-    print('Kiruu bolumundosuz!');
+      stdout.write("Syr soz jazynyz : ");
+      var passoword = stdin.readLineSync();
 
-    stdout.write('Login jazynyz : ');
-    var login = stdin.readLineSync();
+      // Fixme: bulardy irettesh kerek
+      print('Name is $name');
+      print('Lastname is $lastName');
+      print('Age is $age');
+      print('Login is $login');
+      print('Pasword is $passoword'); // Fixme (passiword korunbosh kerek)
+      // Todo(edliiar): userdi saktoo
 
-    stdout.write('Syr sozunuzdu jazynyz : ');
-    var password = stdin.readLineSync();
+      var newUser = {
+        'name': name,
+        'lastname': lastName,
+        'age': age,
+        'login': login,
+        'password': passoword,
+      };
 
-    if (login == user['login'] && password == user['password']) {
-      print('Uraaa');
+      userler.add(newUser);
+
+      stdout.write("Chyguu uchun 1  ulantuu enter kiriniz: ");
+      quit = stdin.readLineSync() ?? '';
+
+      print('Siz iygiliktuu kattaldynyz :)');
+    } else if (value == 'N') {
+      print('Kiruu bolumundosuz!');
+
+      stdout.write('Login jazynyz : ');
+      var login = stdin.readLineSync();
+
+      stdout.write('Syr sozunuzdu jazynyz : ');
+      var password = stdin.readLineSync();
+
+      for (var user in userler) {
+        if (login == user['login'] && password == user['password']) {
+          print('Uraaa: $user');
+        } else {
+          print('Login je syr soz kata');
+        }
+      }
+
+      stdout.write("Chyguu uchun 1  ulantuu enter kiriniz: ");
+      quit = stdin.readLineSync() ?? '';
     } else {
-      print('Login je syr soz kata');
+      print('Kechiresiz berilgen maalymat KATA! Suranych tuura jazynyz');
     }
-  } else {
-    print('Kechiresiz berilgen maalymat KATA! Suranych tuura jazynyz');
-  }
+  } while (quit != '1');
 }
