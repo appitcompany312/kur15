cf32ae17a39f783efade8f803b38034819df7476
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-11-17 00:09:59 +0600

    added 39-lesson news app getx

--- news_app_with_getx/lib/news/controller/news_controller.dart
+++ news_app_with_getx/lib/news/controller/news_controller.dart
@@ -1,7 +1,10 @@
 // bul jakta news view du kozomoldogon controller jazylat
 
+import 'dart:convert';
+
 import 'package:get/get.dart';
 import 'package:http/http.dart';
+import 'package:news_app_with_getx/models/news_data.dart';
 
 const apiLink = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=5ea6b14fb6084c9dae2f9a00f75cb044';
 
@@ -13,6 +16,7 @@ class NewsController extends GetxController {
   RxBool isLoading = false.obs;
   RxString data = ''.obs;
   RxString error = ''.obs;
+  Rx<NewsData?> newsData = Rx<NewsData?>(null);
 
   Future<void> getNews() async {
     try {
@@ -22,6 +26,8 @@ class NewsController extends GetxController {
       print(response);
       isLoading.value = false;
       if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body) as Map<String, dynamic>;
+        newsData.value = NewsData.fromJson(decodedData);
         data.value = response.body;
         print(data.value);
       } else {
--- news_app_with_getx/lib/news/view/news_page.dart
+++ news_app_with_getx/lib/news/view/news_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/state_manager.dart';
 import 'package:news_app_with_getx/news/controller/news_controller.dart';
+import 'package:news_app_with_getx/widgets/news_card.dart';
 
 class NewsPage extends StatelessWidget {
   const NewsPage(this.controller, {super.key});
@@ -14,18 +15,27 @@ class NewsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('News'),
       ),
-      body: Center(
-        child: Obx(() {
-          if (controller.isLoading.value && controller.data.value.isEmpty && controller.error.value.isEmpty) {
-            return const CircularProgressIndicator();
-          } else if (controller.data.value.isNotEmpty) {
-            return Text(controller.data.value);
-          } else if (controller.error.value.isNotEmpty) {
-            return Text(controller.error.value);
-          } else {
-            return const SizedBox.shrink();
-          }
-        }),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 30),
+        child: Center(
+          child: Obx(() {
+            if (controller.isLoading.value && controller.data.value.isEmpty && controller.error.value.isEmpty) {
+              return const CircularProgressIndicator();
+            } else if (controller.newsData.value != null) {
+              return ListView.builder(
+                itemCount: controller.newsData.value!.articles.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final article = controller.newsData.value!.articles[index];
+                  return NewsCard(article);
+                },
+              );
+            } else if (controller.error.value.isNotEmpty) {
+              return Text(controller.error.value);
+            } else {
+              return const SizedBox.shrink();
+            }
+          }),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
