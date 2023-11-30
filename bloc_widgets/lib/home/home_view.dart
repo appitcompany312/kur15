29609746ec45b996d84281e5cea7797fa8b3cf78
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-12-01 00:25:31 +0600

    added posts_bloc infinity

--- bloc_widgets/lib/home/home_view.dart
+++ bloc_widgets/lib/home/home_view.dart
@@ -13,49 +13,53 @@ class HomeView extends StatelessWidget {
       ),
       // `BlocListener` - <> berilgen cubit je bloctu ugup turat jana
       // state ke jarasha berilgen function dy atkarat
-      body: BlocListener<HomeCubit, HomeState>(
-        listener: (context, state) {
-          if (state is HomeErrorState) {
-            showAdaptiveDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: const Text('Oop! Error value'),
-                content: Text('Value is ${state.message}'),
-              ),
-            );
-          }
-        },
-        child: Center(
-          child: Column(
-            children: [
-              // BlocBuilder -  <> berilgen cubit je bloc menen state ugup turat
-              // state ke jarasha ozun kurat
-              BlocBuilder<HomeCubit, HomeState>(
-                builder: (context, state) {
-                  if (state is HomeInitialState) {
-                    return const Text('Initial');
-                  } else if (state is HomeLoadingState) {
-                    return const CircularProgressIndicator();
-                  } else if (state is HomeSuccessState) {
-                    return Text('Success: ${state.data}');
-                  } else if (state is HomeErrorState) {
-                    return Text('Error: ${state.message}');
-                  } else {
-                    return const Text('Unknown');
-                  }
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // context.read<HomeCubit>(); - <> berilgen objecti
-                  // beret eger BlocProvider menen berilgen bolso
-                  final homeCubit = context.read<HomeCubit>();
-                  homeCubit.parseNumber('234wed');
-                },
-                child: const Text('parse'),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          children: [
+            // BlocBuilder -  <> berilgen cubit je bloc menen state ugup turat
+            // state ke jarasha ozun kurat
+            BlocConsumer<HomeCubit, HomeState>(
+              builder: (context, state) {
+                if (state is HomeInitialState) {
+                  return const Text('Initial');
+                } else if (state is HomeLoadingState) {
+                  return const CircularProgressIndicator();
+                } else if (state is HomeSuccessState) {
+                  return Text('Success: ${state.data}');
+                } else if (state is HomeErrorState) {
+                  return Text('Error: ${state.message}');
+                } else {
+                  return const Text('Unknown');
+                }
+              },
+              listener: (BuildContext context, HomeState state) {
+                if (state is HomeErrorState) {
+                  showAdaptiveDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('Oop! Error value'),
+                      content: Text('Value is ${state.message}'),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text('Ok'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // context.read<HomeCubit>(); - <> berilgen objecti
+                // beret eger BlocProvider menen berilgen bolso
+                final homeCubit = context.read<HomeCubit>();
+                homeCubit.parseNumber('234wed');
+              },
+              child: const Text('parse'),
+            ),
+          ],
         ),
       ),
     );
