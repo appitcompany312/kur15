c80adc6fc8e1ad43d0746b8ce1539aca283a96ca
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-12-11 23:10:30 +0600

    first chat app commit

---
+++ chat_app/doc/useage.dart
@@ -0,0 +1,9 @@
+/*
+1) Flutter (mobile)
+2) Assets (icons, images)
+3) Animated_text_kit (animation)
+4) Bloc, flutter_bloc  (state management)
+5) Firebase (cloud service, backend)
+6) Firebase Auth (login and register)
+7) Firebase Firestore (database)
+*/
