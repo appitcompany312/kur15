6c7a1945b7fa7580e2448def26c2de7942aab491
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-12-15 00:06:47 +0600

    created auth bloc

--- chat_app/lib/home/view/home_view.dart
+++ chat_app/lib/home/view/home_view.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/app/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,7 +14,15 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is AuthenticatedState) {
+              return Text(state.user.email);
+            } else {
+              return const Text('Home');
+            }
+          },
+        ),
       ),
     );
   }
